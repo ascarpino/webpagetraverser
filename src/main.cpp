88b78ec624a773e0b90b9c64928fbea33858372b
Andrea Scarpino <andrea@datatoknowledge.it>
2014-06-02 11:08:07 +0200

    Add --json and --filename options

--- src/main.cpp
+++ src/main.cpp
@@ -43,10 +43,10 @@ int main(int argc, char *argv[])
     parser.addVersionOption();
     parser.addPositionalArgument("url", "Webpage URL.");
 
-    QCommandLineOption destFileName("f", "Redirect the output to a file.", "filename");
+    QCommandLineOption destFileName(QStringList() << "f" << "filename", "Redirect the output to a file.", "filename");
     parser.addOption(destFileName);
 
-    QCommandLineOption useJson("j", "Use JSON output instead of plain text.");
+    QCommandLineOption useJson(QStringList() << "j" << "json", "Use JSON output instead of plain text.");
     parser.addOption(useJson);
 
     parser.process(a);
